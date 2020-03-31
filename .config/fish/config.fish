a6c40dce6899966964e545ea8c979a07ef972481
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-03-31 21:43:51 +0900

    [add]config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
+set -x PATH /home/takumi/go/bin $PATH
+set -x PATH ~/.local/bin $PATH
+set -x PATH $PATH $HOME/.krew/bin
+set GHQ_SELECTOR peco
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_display_git_master_branch yes
+set -g theme_display_k8s_context yes
+set -g theme_title_display_process yes
+set -g theme_newline_cursor yes
