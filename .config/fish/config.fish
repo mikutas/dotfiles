f56df0bf3f132546649b612e18468dbe7d62063a
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-04-14 16:49:14 +0900

    [add]bobthefish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set -x PATH ~/.local/bin $PATH
 set -x PATH $PATH $HOME/.krew/bin
 set GHQ_SELECTOR peco
 set -g fish_prompt_pwd_dir_length 0
+set -g theme_project_dir_length 0
 set -g theme_display_git_master_branch yes
 set -g theme_display_k8s_context yes
 set -g theme_title_display_process yes
