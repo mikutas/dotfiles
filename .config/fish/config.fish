a3cf4d162ac3c5e46b0c327da8cd453ba7efc683
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-04-21 13:36:39 +0900

    [add]starship config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,3 +17,5 @@ set -g theme_newline_cursor yes
 set -g theme_show_exit_status yes
 set -g theme_title_display_path no
 set -g theme_title_display_process yes
+# starship
+#starship init fish | source
--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 ln -s (pwd)/.config/fish/config.fish ~/.config/fish/config.fish
 ln -s (pwd)/.config/fish/aliases.fish ~/.config/fish/aliases.fish
+ln -s (pwd)/.config/starship.toml ~/.config/starship.toml
