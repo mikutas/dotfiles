7c432d4a35939667c42ad8cc005ad0af813fbc01
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-04-22 10:23:49 +0900

    [update]GHQ_SELECTORをグローバル変数に
    ほかコメント

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,14 @@
 . ~/.config/fish/aliases.fish
+# PATH
 set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
 set -x PATH ~/.local/bin $PATH
 set -x PATH $PATH $HOME/.krew/bin
 set -x GOPATH (go env GOPATH)
 set -x PATH $GOPATH/bin $PATH
+# https://jlk.fjfi.cvut.cz/arch/manpages/man/gpg-agent.1.html
 set -x GPG_TTY (tty)
-set GHQ_SELECTOR peco
+# fish-ghq
+set -g GHQ_SELECTOR peco
 # bobthefish
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 0
