65f6f6cae75a09a2de894af446aac86c4c2147d9
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-04-24 22:59:08 +0900

    [add]gpg-agent.conf

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 ln -s (pwd)/.config/fish/config.fish ~/.config/fish/config.fish
 ln -s (pwd)/.config/fish/aliases.fish ~/.config/fish/aliases.fish
 ln -s (pwd)/.config/starship.toml ~/.config/starship.toml
+ln -s (pwd)/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
