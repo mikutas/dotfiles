61572ff4650de71885e3347a8019c3b1a3684b3f
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-12-14 11:48:03 +0900

    fix: kubectl

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -75,7 +75,7 @@ function kubectl
 	set KUBE1_18 ~/.asdf/installs/kubectl/1.18.12/bin/kubectl
 	set KUBE1_19 ~/.asdf/installs/kubectl/1.19.5/bin/kubectl
 
-	set VERSION ($KUBE1_19 version --short | tail -n 1 | sed -r "s/^Server Version: v([0-9].[0-9][0-9]).[0-9]/\1/")
+	set VERSION ($KUBE1_19 version --short | tail -n 1 | sed -r "s/^Server Version: v([0-9].[0-9][0-9]).[0-9].*/\1/")
 
 	if test (string match $VERSION "1.15")
 		$KUBE1_15 $argv;
@@ -87,6 +87,8 @@ function kubectl
 		$KUBE1_18 $argv;
 	else if test (string match $VERSION "1.19")
 		$KUBE1_19 $argv;
+	else
+		echo "Debug: VERSION=$VERSION"
 	end
 end
 
