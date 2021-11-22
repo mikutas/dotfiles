03550c9bc77d6a6728268d148de68d365530b2ef
mikutas <23391543+mikutas@users.noreply.github.com>
2021-11-22 13:04:16 +0900

    feat: asdf version config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s (pwd)/.config/fish/config.fish ~/.config/fish/config.fish
 ln -s (pwd)/.config/fish/aliases.fish ~/.config/fish/aliases.fish
 ln -s (pwd)/.config/starship.toml ~/.config/starship.toml
 ln -s (pwd)/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+ln -s (pwd)/.tool-versions ~/.tool-versions
